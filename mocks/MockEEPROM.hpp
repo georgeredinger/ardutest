8d12d82abbf1b53cdd2c910bba72d1e83325d200
Nick Pascucci <npascut1@gmail.com>
2011-10-18 17:00:18 -0600

    Added LGPL and GPL, and associated headers.

--- mocks/MockEEPROM.hpp
+++ mocks/MockEEPROM.hpp
@@ -1,3 +1,27 @@
+/*
+    Ardutest - An Arduino mocks library for unit testing.  
+
+    Copyright (C) 2011 Mind Studios, Inc.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation; either version 2.1 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+    for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+    Mind Studios, Inc., hereby disclaims all copyright interest in the library
+    Ardutest (an Arduino mocks library for unit testing) written by Nick
+    Pascucci.
+ */
+
 /*!
   \file
   Mock EEPROM functionality for unit testing.
